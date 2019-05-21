d1d96a700e82e8cbfe2c9e057cab645654f7dcd5
daniela-idara <purpleelephant1987@gmail.com>
2019-05-20 20:28:46 -0500

    loads all users

--- windowLoad/app.js
+++ windowLoad/app.js
@@ -1,32 +1,46 @@
-const newArray = [];
+let userId = 0;
 
-function get(){
+
+
+(function () {
+    const userArray = [];
+    const allUsers = document.querySelector('#users');
+
+    class User {
+      constructor(name, image, email, phone, birth) {
+        this.userId = userId++;
+        this.name = name;
+        this.image = image;
+        this.email = email;
+        this.phone = phone;
+        this.birth = birth;
+      }
+    }
+
     fetch('https://randomuser.me/api/?results=20')
       .then( response => response.json())
       .then( data => {
+      
         
-        newArray.push(data);
-        console.log(newArray);
-        const listUsers = [];
-        
-        for (let i=0; i<newArray.length; i++){
+        data.results.forEach(function(element) {
           
-          const name = newArray[i].results['0'].name.first + " " + newArray[i].results['0'].name.last;
-          const image = newArray[i].results['0'].picture.large;
-          const email = newArray[i].results['0'].email;
-          const phone = newArray[i].results["0"].phone;
-          const birth = newArray[i].results["0"].dob.date.toString().slice(0,10);
-
-
-          listUsers.push({
-            'name': newArray[i].results['0'].name.first + " " + newArray[i].results['0'].name.last,
-            'image': newArray[i].results['0'].picture.large,
-            'email': newArray[i].results['0'].email,
-            'number': newArray[i].results["0"].phone,
-            'dob': newArray[i].results["0"].dob
+          const name = element.name.first + " " + element.name.last;
+          const image = element.picture.large;
+          const email = element.email;
+          const phone = element.phone;
+          const birth = element.dob.date.toString().slice(0,10);
+          userArray.push(new User(name, image, email, phone, birth));
+          createDom(name, image, email, phone, birth);    
           });
-          
-          allUsers.innerHTML += `
+          console.log(userArray);
+        })
+      
+      .catch( error => console.log("oops, looks like we got an error: ", error))
+      .finally( ()=> console.log("finally, This function always runs...")) // Whether or not there's an error or success, this will happen such as stopping a loading wheel on the front end
+      // allUsers.innerHTML = "";
+      
+      const createDom = (name, image, email, phone, birth) => {
+        allUsers.innerHTML += `
               <div style='display:inline-block'; class='eachUser'>
                 <div style="height:320px">
                 <br>
@@ -46,88 +60,16 @@ function get(){
               </div>
             `
         }
-        console.log(listUsers);
-        
-      })
-      
-      .catch( error => console.log("oops, looks like we got an error: ", error))
-      .finally( ()=> console.log("finally, This function always runs...")) // Whether or not there's an error or success, this will happen such as stopping a loading wheel on the front end
-      allUsers.innerHTML = "";
-      
-    }
-    
-    let multipleArray = [];
-    function getAllUsers(){
-      fetch('https://randomuser.me/api/?results=20')
-        .then( response => response.json())
-        .then( data => {
-          
-          newArray.push(data);
-          console.log(newArray);
-          const listUsers = [];
-          
-          for (let i=0; i<multipleArray.length; i++){
-            
-            const name = newArray[i].results['0'].name.first + " " + newArray[i].results['0'].name.last;
-            const image = newArray[i].results['0'].picture.large;
-            const email = newArray[i].results['0'].email;
-            const phone = newArray[i].results["0"].phone;
-            const birth = newArray[i].results["0"].dob.date.toString().slice(0,10);
   
-  
-            multipleArray.push({
-              'name': newArray[i].results[i].name.first + " " + newArray[i].results['0'].name.last,
-              'image': newArray[i].results['0'].picture.large,
-              'email': newArray[i].results['0'].email,
-              'number': newArray[i].results["0"].phone,
-              'dob': newArray[i].results["0"].dob
-            });
-            
-            allUsers.innerHTML += `
-                <div style='display:inline-block'; class='eachUser'>
-                  <div style="height:320px">
-                  <br>
-                  <span id="name">${name}</span>
-                  <br><br>
-                  <img src='${image}' height = "250px"
-                  width = "250px" class="image center" onmouseover='getInfo(this)' 
-                  />
-                  <div class="middle">
-                  <div class="text"> 
-                  &#128222 ${phone}
-                  <br>
-                  &#x2709; ${email}
-                  <br>
-                  &#127874; ${birth} </div>
-                  </div>
-                </div>
-              `
-          }
-          console.log(listUsers);
-          
-        })
-        
-        .catch( error => console.log("oops, looks like we got an error: ", error))
-        .finally( ()=> console.log("finally, This function always runs...")) // Whether or not there's an error or success, this will happen such as stopping a loading wheel on the front end
-        allUsers.innerHTML = "";
-        
-      }
-      getAllUsers();
-      getAllUsers();
-    allUsers.innerHTML = "";
-
-    function getInfo(x){
-      x.style.display = "block";   
-    }
+    })()
     
-    function delayFunction () {
-      setTimeout(function(){get();}, 800);
+      function getInfo(x){
+        x.style.display = "block";   
       }
-      delayFunction();
+     
 
-    window.onload = function() {
-      getAllUsers();
-  }
+    
+  
  
 
    
